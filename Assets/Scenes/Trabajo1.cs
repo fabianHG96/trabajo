19769e87b180b5663018d255db38c75188fe6421
fabianHG96 <fabian.huenchulaf@virginiogomez.cl>
2022-09-27 21:23:55 -0300

    se agrego el pryecto avanzado xd

---
+++ Assets/Scenes/Trabajo1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WallOrientation
+{
+    WEST=0,NORTH=1,EAST=2,SOUTH=3
+}
+public class Trabajo1 : MonoBehaviour
+{
+  
+}
---
+++ Assets/Scripts/Tank/destruir.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destruir : MonoBehaviour
+{
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+   
+}
